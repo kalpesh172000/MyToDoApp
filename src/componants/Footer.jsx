ceae6a8dbf163de8d0edef3b8355ee3aafa2685f
kalpesh <kalpesh200071@gmail.com>
2024-06-19 08:24:36 +0530

    added header and footer

---
+++ src/componants/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <>
+            <footer>
+                <p>Copyright &copy; {new Date().getFullYear()}</p>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
