e2639b1b244f54ae12d1952ff86757636f08277d
kalpesh <kalpesh200071@gmail.com>
2024-06-19 18:43:51 +0530

    added way to dynamically add the new entry to list

--- src/componants/Note.jsx
+++ src/componants/Note.jsx
@@ -1,11 +1,8 @@
-function Note()
-{
-    return(
-        <div className="note">
-            <h1>This is the note title</h1>
-            <p>This is the note content</p>
-        </div>
-    )
+import notes from "../notes";
+import Entry from "./Entry";
+
+function Note() {
+    return notes.map((notes) => <Entry key={notes.id} title={notes.title} content={notes.content} />);
 }
 
 export default Note;
